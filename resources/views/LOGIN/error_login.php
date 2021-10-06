b61caf5c21dd559a40d2bca6a769fcd6ede74c68
Babyslam <rezaldy.rafi2@gmail.com>
2021-10-06 23:12:05 +0700

    style error login

--- resources/views/LOGIN/error_login.php
+++ resources/views/LOGIN/error_login.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/styleregisteragen.css">
+	<link rel="stylesheet" type="text/css" href="css/styleerrorlogin.css">
 	<script src="/public/js/javascriptagen.js"></script>
 	<title>LOGIN ERROR</title>
 
